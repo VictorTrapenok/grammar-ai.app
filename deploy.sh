0d2893bab11aa6886a6b51adae4cf2750611b1d2
TrapenokVictor <Levhav@yandex.com>
2025-10-23 10:10:28 +0300

    Add

--- deploy.sh
+++ deploy.sh
@@ -10,7 +10,7 @@ echo "Build done"
 
 rm -rf ./app-ai
 
-git clone https://https://github.com/VictorTrapenok/grammar-ai.app.git -b gh-pages
+git clone https://github.com/VictorTrapenok/grammar-ai.app.git -b gh-pages
 
 mv ./app-ai/.git ./build/.git
 rm -rf ./app-ai
